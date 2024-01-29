b9ecec174dd67e82ca516cb8e7e584d0c775fdbf
GeorgeHolas <georgeholas404@gmail.com>
2024-01-29 09:46:17 -0600

    Minor fixes

--- client/src/components/LogoutMessage/logoutMessage.js
+++ client/src/components/LogoutMessage/logoutMessage.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import styles from './logoutMessage.module.css';
 
+// LogoutMessage component
 const LogoutMessage = ({ message }) => {
   return (
     <div className={styles.logoutMessageContainer}>
