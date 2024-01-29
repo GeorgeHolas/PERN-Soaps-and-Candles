fc745ebd88cce5e6899c30d3a786ae26f1051b5a
GeorgeHolas <georgeholas404@gmail.com>
2024-01-29 10:12:06 -0600

    readability

--- client/src/routes/Products/products.js
+++ client/src/routes/Products/products.js
@@ -1,8 +1,9 @@
 // Products.js
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import styles from './products.module.css';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import styles from "./products.module.css";
 
+// Product component
 const Product = ({ product }) => {
   return (
     <Link to={`/products/${product.Product_id}`} className={styles.productLink}>
@@ -11,37 +12,36 @@ const Product = ({ product }) => {
           <img
             src={process.env.REACT_APP_IMAGE_PATH + `/${product.imageName}`}
             alt={`Product: ${product.Name}`}
-            style={{ maxWidth: '400px' }}
+            style={{ maxWidth: "400px" }}
           />
         </div>
         <div className={styles.productDetails}>
           <h3>{product.name}</h3>
           <p>{product.description}</p>
-          <div className={styles.detailsLink}>
-            View Details
-          </div>
+          <div className={styles.detailsLink}>View Details</div>
         </div>
       </div>
     </Link>
   );
 };
 
+// ProductList component
 const ProductList = ({ title, category }) => {
   const [products, setProducts] = useState([]);
 
+  // Fetch products from the API endpoint
   useEffect(() => {
-    // Fetch products from the API endpoint
     fetch(`http://localhost:4000/products?category=${category}`)
-      .then(response => response.json())
-      .then(data => setProducts(data))
-      .catch(error => console.error('Error fetching products:', error));
+      .then((response) => response.json())
+      .then((data) => setProducts(data))
+      .catch((error) => console.error("Error fetching products:", error));
   }, [category]);
 
   return (
     <div>
       <h2 className={styles.productTitle}>{title}</h2>
       <div className={styles.productcontainer}>
-        {products.map(product => (
+        {products.map((product) => (
           <Product key={product.Product_id} product={product} />
         ))}
       </div>
