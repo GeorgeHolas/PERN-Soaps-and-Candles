2b7d8a43196dd040775d71828b188f4d3f485b86
GeorgeHolas <georgeholas404@gmail.com>
2024-01-29 10:10:45 -0600

    Minor changes to readability

--- client/src/routes/ProductDetails/ProductDetails.js
+++ client/src/routes/ProductDetails/ProductDetails.js
@@ -1,79 +1,85 @@
 // ProductDetails.js
-import React, { useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
-import axios from 'axios';
-import styles from './ProductDetails.module.css';
+import React, { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import axios from "axios";
+import styles from "./ProductDetails.module.css";
 
 // Add products to a cart
 const ProductDetails = ({ onAddToCart }) => {
-  const { productId } = useParams();
-  const [product, setProduct] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [addToCartMessage, setAddToCartMessage] = useState(null);
+const { productId } = useParams();
+const [product, setProduct] = useState(null);
+const [loading, setLoading] = useState(true);
+const [error, setError] = useState(null);
+const [addToCartMessage, setAddToCartMessage] = useState(null);
 
-  useEffect(() => {
-    const fetchProductDetails = async () => {
-      try {
-        if (!productId) {
-          throw new Error('Product ID is missing');
-        }
-
-        const response = await axios.get(`http://localhost:4000/products/${productId}`);
-        setProduct({ ...response.data, quantity: 1 });
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
+// Get product details
+useEffect(() => {
+  const fetchProductDetails = async () => {
+    try {
+      if (!productId) {
+        throw new Error("Product ID is missing");
       }
-    };
-
-    fetchProductDetails();
-  }, [productId]);
-
-  const handleAddToCart = () => {
-    onAddToCart(product);
-    setAddToCartMessage('Item added to the cart!');
+      // Get product details from the database
+      const response = await axios.get(
+        `http://localhost:4000/products/${productId}`
+      );
+      setProduct({ ...response.data, quantity: 1 });
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
   };
 
-  // Loading state
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  fetchProductDetails();
+}, [productId]);
 
-  // Error state
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
+// Add to cart message
+const handleAddToCart = () => {
+  onAddToCart(product);
+  setAddToCartMessage("Item added to the cart!");
+};
 
-  // Product not found state
-  if (!product) {
-    return <div>Product not found</div>;
-  }
+// Loading state
+if (loading) {
+  return <div>Loading...</div>;
+}
 
-  return (
-    <><div className={styles.mainContainer}>
-        <img className={styles.productImage}
-          src={process.env.REACT_APP_IMAGE_PATH + `/${product.imageName}`}
-          alt={`Product: ${product.Name}`} />
+// Error state
+if (error) {
+  return <div>Error: {error}</div>;
+}
+
+// Product not found state
+if (!product) {
+  return <div>Product not found</div>;
+}
+
+return (
+  <>
+    <div className={styles.mainContainer}>
+      <img
+        className={styles.productImage}
+        src={process.env.REACT_APP_IMAGE_PATH + `/${product.imageName}`}
+        alt={`Product: ${product.Name}`}
+      />
       <div className={styles.productDetailsContainer}>
         <div className={styles.productDetails}>
           <h2>{product.Name}</h2>
           <p>{product.Description}</p>
         </div>
-        </div>
-        </div>
-        <div className={styles.priceContainer}>
-          <p className={styles.price}>Price: ${product.Price}</p>
-          <button onClick={handleAddToCart}>Add to Cart</button>
-          {addToCartMessage && <p>{addToCartMessage}</p>}
-          <Link to="/Products" className={styles.returnLink}>
-            <p>Return to Products</p>
-          </Link>
-        </div>
-      </>
-    
-  );
+      </div>
+    </div>
+    <div className={styles.priceContainer}>
+      <p className={styles.price}>Price: ${product.Price}</p>
+      <button onClick={handleAddToCart}>Add to Cart</button>
+      {addToCartMessage && <p>{addToCartMessage}</p>}
+      <Link to="/Products" className={styles.returnLink}>
+        <p>Return to Products</p>
+      </Link>
+    </div>
+  </>
+);
 };
 
 export default ProductDetails;
