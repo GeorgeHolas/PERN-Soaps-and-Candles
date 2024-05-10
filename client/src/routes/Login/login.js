ce4206839c0e0221c89a1d720ca2c361c1be9d51
Jiri Holas <130999425+GeorgeHolas@users.noreply.github.com>
2024-05-09 21:41:50 -0600

    Update for render login.js

--- client/src/routes/Login/login.js
+++ client/src/routes/Login/login.js
@@ -19,7 +19,7 @@ function Login() {
 
   const onSubmit = async (data) => {
     try {
-      const response = await fetch("http://localhost:4000/auth/login", {
+      const response = await fetch(`${backendUrl}/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
