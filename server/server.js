e175aa535c4d1a55245a9a89f25bd848a71ba860
Jiri Holas <130999425+GeorgeHolas@users.noreply.github.com>
2024-05-09 21:59:59 -0600

    Update for render server.js

--- server/server.js
+++ server/server.js
@@ -44,6 +44,14 @@ app.use(
     credentials: true,
   })
 );
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://soaps-and-candles-frontend.onrender.com");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
